1f17ee4ded7dc9adb5c224fd7d1a36205f27cb93
SHAN-TG <85439835+SHAN-TG@users.noreply.github.com>
2022-08-01 09:58:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -756,6 +756,6 @@ async def advantage_spell_chok(msg):
                 )
             ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data=f'spolling#{user}#close_spellcheck')])
-  m = await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?", reply_markup=InlineKeyboardMarkup(btn))
+  k = await msg.reply("I couldn't find anything related to that\nDid you mean any one of these?", reply_markup=InlineKeyboardMarkup(btn))
   await asyncio.sleep(30)
-  await m.delete()
+  await k.delete()
